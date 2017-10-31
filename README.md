29c06785be8314dc657543a47e74fab556716785
jmichealson <31416532+jmichealson@users.noreply.github.com>
2017-10-31 12:25:51 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spring Config Decorator
+
+This is a [decorator](https://github.com/cf-platform-eng/meta-buildpack/blob/master/README.md#decorators) buildpack
+for Cloud Foundry that adds a Chuck Norris joke in the "JOKE" environment variable *for any programming
+language* supported by the platform, and requiring *zero application code changes*.
+
+# chuck_decorator
---
+++ lib/comic_relief.py
@@ -0,0 +1,29 @@
+# comic-relief-decorator
+#
+# Copyright (c) 2015-Present Pivotal Software, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import urllib2
+import json
+
+def main():
+    key = "JOKE"
+    value = json.loads(urllib2.urlopen("http://api.icndb.com/jokes/random").read())['value']['joke']
+    print key, value
+
+def detect():
+    print 'comic-relief'
+
+if __name__ == "__main__":
+    main()
